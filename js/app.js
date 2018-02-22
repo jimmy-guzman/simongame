2865fc66b2b9e3ad0d1664889b4aebc6039f5dcf
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-02-21 19:08:54 -0600

    working prototype

--- js/app.js
+++ js/app.js
@@ -2,29 +2,33 @@ const boxes = document.querySelectorAll(".box");
 const startGameButton = document.getElementById("startGame");
 const counter = document.querySelector(".counter p");
 
-const initSeq = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3];
+// const initSeq = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3];
+const initSeq = [0, 1, 2];
+let rSeq = [];
 let count = 0;
 let picks = [];
 let pickCount = 0;
+let isPlayerTurn = false;
+let maxSteps = 1;
 
 function randSeq() {
   return initSeq.map(org => Math.floor(Math.random() * Math.floor(4)));
 }
 
-function startSeq(seq, upTo) {
+function startSeq() {
+  isPlayerTurn = false;
   setTimeout(function() {
-    step(seq, count);
+    step(count);
     count++;
     updateCounter(count);
-    if (count < upTo) startSeq(seq, upTo);
+    if (count < maxSteps) startSeq();
+    playerTurn();
   }, 1500);
 }
 
-function step(seq, nextStep) {
-  console.log(nextStep);
-  boxes[seq[nextStep]].classList.add("glow");
-  let audioURL = `sounds/sound${seq[nextStep] + 1}.wav`;
-  playAudio(audioURL);
+function step(nextStep) {
+  boxes[rSeq[nextStep]].classList.add("glow");
+  playAudio(`sounds/sound${rSeq[nextStep] + 1}.wav`);
   isNext = false;
 }
 
@@ -33,32 +37,68 @@ function playAudio(audioURL) {
   audio.play();
 }
 
-function simonTurn(seq) {
-  startSeq(seq, 20);
-  playerTurn();
+function simonTurn() {
+  startSeq();
+  // if (isPlayerTurn) playerTurn(maxSteps);
 }
 
 function playerTurn() {
-  boxes.forEach(box => box.addEventListener("click", pick));
+  // console.log(pickCount, count, picks, maxSteps);
+  if (count + 1 > maxSteps) {
+    isPlayerTurn = true;
+  }
 }
 
 function pick() {
   const boxesArr = Array.from(boxes);
-  if(pickCount < count) {
-    const index = boxesArr.indexOf(this);
+  let index = 0;
+  if (pickCount < count && isPlayerTurn) {
+    index = boxesArr.indexOf(this);
     boxes[index].classList.add("glow");
-    let audioURL = `sounds/sound${index + 1}.wav`;
-    playAudio(audioURL);
     picks.push(boxesArr.indexOf(this));
-    console.log(picks);
   }
-  pickCount++;
+
+  checkIfCorrect(picks, index);
+  // console.log("pick count: ", pickCount);
+}
+
+function checkIfCorrect(picks, index) {
+  // console.log("picks: ", picks, rSeq[pickCount]);
+  
+  if (picks[pickCount] === rSeq[pickCount]) {
+    pickCount++;
+    console.log("correct");
+    playAudio(`sounds/sound${index + 1}.wav`);
+    if(maxSteps === rSeq.length && pickCount === rSeq.length) {
+      playAudio("sounds/applause.wav");
+      playAudio(audioURL);
+      isPlayerTurn = false;
+    }
+    else if(pickCount === maxSteps) {
+      pickCount = 0;
+      console.log("next round");
+      count = 0;
+      picks.length = 0;
+      maxSteps++;
+      simonTurn();
+    }
+  } else {
+    pickCount = 0;
+    playAudio("sounds/error.wav");
+    count = 0;
+    picks.length = 0;
+    simonTurn();
+  }
 }
 
 function startGame() {
-  const seq = randSeq();
-  console.log(seq);
-  simonTurn(seq);
+  rSeq = randSeq();
+  console.log(rSeq);
+  pickCount = 0;
+  count = 0;
+  picks.length = 0;
+  maxSteps = 1;
+  simonTurn();
 }
 
 function removeTransition(e) {
@@ -70,5 +110,6 @@ function updateCounter(steps) {
   counter.textContent = steps;
 }
 
+boxes.forEach(box => box.addEventListener("click", pick));
 boxes.forEach(box => box.addEventListener("transitionend", removeTransition));
 startGameButton.addEventListener("click", startGame);
